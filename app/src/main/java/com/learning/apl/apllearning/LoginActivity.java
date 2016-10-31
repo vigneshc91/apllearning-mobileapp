0471c3d10eae304aac290ac2e1dd16368a3dcf11
Vignesh C <vigneshc@wekancode.com>
2016-10-31 23:22:12 +0530

    login page designed

--- app/src/main/java/com/learning/apl/apllearning/LoginActivity.java
+++ app/src/main/java/com/learning/apl/apllearning/LoginActivity.java
@@ -2,12 +2,34 @@ package com.learning.apl.apllearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText userNameText;
+    private EditText passwordText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameText = (EditText) findViewById(R.id.userName);
+        passwordText = (EditText) findViewById(R.id.userPassword);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userNameString = userNameText.getText().toString();
+                String passwordString = passwordText.getText().toString();
+
+                Toast.makeText(LoginActivity.this, "Login Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
